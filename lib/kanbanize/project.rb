d90fb62e92bf18daf8f51aff59ff3bad4e861e4a
Renaud Martinet <karouf@gmail.com>
2013-07-01 15:30:40 +0200

    Get a project's boards

--- lib/kanbanize/project.rb
+++ lib/kanbanize/project.rb
@@ -1,10 +1,17 @@
 module Kanbanize
   class Project
-    attr_reader :id, :name
+    attr_reader :id, :name, :boards
 
     def initialize(data)
       @id = data['id'].to_i
       @name = data['name']
+
+      @boards = []
+      if data['boards']
+        data['boards'].each do |board|
+          @boards << Board.new(board)
+        end
+      end
     end
   end
 end
--- spec/lib/kanbanize/project_spec.rb
+++ spec/lib/kanbanize/project_spec.rb
@@ -9,11 +9,33 @@ describe Kanbanize::Project do
     Kanbanize::Project.new({}).must_respond_to :name
   end
 
+  it 'gives access to its boards' do
+    Kanbanize::Project.new({}).must_respond_to :boards
+  end
+
   it 'sets its id from the data provided' do
     Kanbanize::Project.new({'id' => '1'}).id.must_equal 1
   end
 
   it 'sets its name from the data provided' do
     Kanbanize::Project.new({'name' => 1}).name.must_equal 1
+  end
+
+  it 'sets its boards from the data provided' do
+    Kanbanize::Project.new({'boards' => [{'name' => 'Testers board', 'id' => '1'}]}).boards.count.must_equal 1
+  end
+
+  describe '#boards' do
+    it 'returns an array' do
+      Kanbanize::Project.new({'boards' => [{'name' => 'Testers board', 'id' => '1'}]}).boards.must_be_instance_of Array
+    end
+
+    it 'returns an array of Board objects' do
+      Kanbanize::Project.new({'boards' => [{'name' => 'Testers board', 'id' => '1'}]}).boards.first.must_be_instance_of Kanbanize::Board
+    end
+
+    it 'returns Board objects belonging to that Project' do
+      Kanbanize::Project.new({'boards' => [{'name' => 'Testers board', 'id' => '1'}]}).boards.first.name.must_equal 'Testers board'
+    end
   end
 end
