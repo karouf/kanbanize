307e8d5bdd38b63d9e1f5109ca2730f9c4e003ce
Renaud Martinet <karouf@gmail.com>
2013-07-01 16:15:18 +0200

    Cleaned up Project specs

--- spec/lib/kanbanize/project_spec.rb
+++ spec/lib/kanbanize/project_spec.rb
@@ -1,41 +1,56 @@
 require_relative '../../spec_helper'
 
 describe Kanbanize::Project do
+
+  subject do
+    Kanbanize::Project.new({  'id' => '1',
+                              'name' => 'Test project',
+                              'boards' => [
+                                            'id' => '1',
+                                            'name' => 'Testers board'
+                                          ]
+                            })
+  end
+
   it 'gives access to its id' do
-    Kanbanize::Project.new({}).must_respond_to :id
+    subject.must_respond_to :id
   end
 
   it 'gives access to its name' do
-    Kanbanize::Project.new({}).must_respond_to :name
+    subject.must_respond_to :name
   end
 
   it 'gives access to its boards' do
-    Kanbanize::Project.new({}).must_respond_to :boards
+    subject.must_respond_to :boards
   end
 
-  it 'sets its id from the data provided' do
-    Kanbanize::Project.new({'id' => '1'}).id.must_equal 1
-  end
+  describe '#initialize' do
+    describe 'with valid data' do
+      it 'sets its id from the data provided' do
+        subject.id.must_equal 1
+      end
 
-  it 'sets its name from the data provided' do
-    Kanbanize::Project.new({'name' => 1}).name.must_equal 1
-  end
+      it 'sets its name from the data provided' do
+        subject.name.must_equal 'Test project'
+      end
 
-  it 'sets its boards from the data provided' do
-    Kanbanize::Project.new({'boards' => [{'name' => 'Testers board', 'id' => '1'}]}).boards.count.must_equal 1
+      it 'sets its boards from the data provided' do
+        subject.boards.count.must_equal 1
+      end
+    end
   end
 
   describe '#boards' do
     it 'returns an array' do
-      Kanbanize::Project.new({'boards' => [{'name' => 'Testers board', 'id' => '1'}]}).boards.must_be_instance_of Array
+      subject.boards.must_be_instance_of Array
     end
 
     it 'returns an array of Board objects' do
-      Kanbanize::Project.new({'boards' => [{'name' => 'Testers board', 'id' => '1'}]}).boards.first.must_be_instance_of Kanbanize::Board
+      subject.boards.first.must_be_instance_of Kanbanize::Board
     end
 
     it 'returns Board objects belonging to that Project' do
-      Kanbanize::Project.new({'boards' => [{'name' => 'Testers board', 'id' => '1'}]}).boards.first.name.must_equal 'Testers board'
+      subject.boards.first.name.must_equal 'Testers board'
     end
   end
 end
